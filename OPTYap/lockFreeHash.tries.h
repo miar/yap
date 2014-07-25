4293b8a662d5daa53108c3b5d7da5dad10fb161f
Miguel Areias <miguel-areias@dcc.fc.up.pt>
2014-07-25 11:46:08 +0100

    integrating the macros defined in opt.macros.h on the external lock-free hash trie files

--- OPTYap/lockFreeHash.tries.h
+++ OPTYap/lockFreeHash.tries.h
@@ -2,39 +2,143 @@
 #define _LOCK_FREE_HASH.TRIES.H
 
 /*******************************************************************************
- *                            compatibility stuff                              *
+ *                            LFHT macros                                      *
+ *******************************************************************************/
+#define LFHT_BIT_SHIFT                               3
+#define LFHT_BUCKET_ARRAY_SIZE                       (1 << LFHT_BIT_SHIFT)
+#define LFHT_MAX_NODES_PER_BUCKET                    4
+#define LFHT_CELL                                    long
+#define LFHT_IsEqualEntry(NODE, ENTRY)               (LFHT_NodeEntry(NODE) == ENTRY)
+#define LFHT_IsHashLevel(PTR)                        ((LFHT_CELL)(PTR) & (LFHT_CELL)(0x1))
+#define LFHT_TagAsHashLevel(PTR) /*v04_tag */          ((LFHT_CELL)(PTR) | (LFHT_CELL)0x1)
+#define LFHT_UntagHashLevel(PTR) /*v04_untag */      ((LFHT_CELL)(PTR) & ~(LFHT_CELL)(0x1))
+
+/*******************************************************************************
+ *                            YapTab compatibility stuff                       *
  *******************************************************************************/
 
 #define LFHT_NodeEntry(NODE)                           TrNode_entry(NODE)
 #define LFHT_NodeNext(NODE)                            TrNode_next(NODE)
+#define LFHT_GetFirstNode(NODE)                        (NODE = (LFHT_STR_PTR) TrNode_child(parent_node))
 #define LFHT_NrLowTagBits                              NumberOfLowTagBits /* 0 (zero) if none */
 #define LFHT_USES_REGS                                 USES_REGS          /* BLANC if no TabMalloc */   
 #define LFHT_PASS_REGS                                 PASS_REGS          /* BLANC if no TabMalloc */ 
 #define LFHT_NODE_ENTRY_STR                            Term
-/*******************************************************************************
- *                            LFHT macros                                      *
- *******************************************************************************/
-#define LFHT_CELL                                    long
-#define LFHT_IsEqualEntry(NODE, ENTRY)               (LFHT_NodeEntry(NODE) == ENTRY)
-#define LFHT_IsHashLevel(PTR)                        ((LFHT_CELL)(PTR) & (LFHT_CELL)(0x1))
 
-/////////////////////////////////////////// ok upto here !!!!!
+/////////////////////////////////////////// ok upto HERE !!!!!
+
+/* integrated with TabMalloc. If no TabMalloc, then use malloc */
+#define V04_ALLOC_THB(STR)						\
+  union trie_hash_buckets *aux;						\
+  ALLOC_STRUCT(aux, union trie_hash_buckets, _pages_trie_hash_buckets); \
+  STR = aux->hash_buckets
+
+
+#define V04_FREE_THB(STR)               //FREE_STRUCT((union trie_hash_buckets*)STR, union trie_hash_buckets, _pages_trie_hash_buckets)
 
 
 
-#define SHIFT_SIZE                                     3
-#define LFHT_BUCKET_SIZE                               (1 << SHIFT_SIZE)
+
+
+#define LFHT_InitBuckets(BUCKET_PTR, PREV_HASH)                         \
+  { int i; void **init_bucket_ptr;                                      \
+    *BUCKET_PTR++ = (void *) (PREV_HASH);                               \
+    init_bucket_ptr = (void **) BUCKET_PTR;                             \
+    for (i = LFHT_BUCKET_ARRAY_SIZE; i != 0; i--)                       \
+      *init_bucket_ptr++ = (void *) LFHT_TagAsHashLevel(BUCKET_PTR);    \
+  }
+
+#if defined(LFHT_LOCAL_THREAD_BUFFER_FOR_BUCKET_ARRAYS)
+#define LFHT_LOCAL_BUCKET_BUFFER           LOCAL_trie_buckets_buffer
+
+#define LFHT_AllocBuckets(BUCKET_PTR, PREV_HASH, STR)	   		                \
+  { void **alloc_bucket_ptr;						                \
+    if(LFHT_LOCAL_BUCKET_BUFFER == NULL) {				                \
+      V04_ALLOC_THB(alloc_bucket_ptr);					                \
+      LFHT_InitBuckets(alloc_bucket_ptr, PREV_HASH);			                \
+    } else {								                \
+      alloc_bucket_ptr = LFHT_LOCAL_BUCKET_BUFFER;		                        \
+      LFHT_LOCAL_BUCKET_BUFFER = NULL;				                        \
+      *alloc_bucket_ptr++ = (void *) (PREV_HASH);			                \
+    }									                \
+    BUCKET_PTR = (STR **) alloc_bucket_ptr;	                	                \
+  }
+
+#define V04_FREE_TRIE_HASH_BUCKETS(PTR, BKT, STR)		               \
+  { V04_SET_HASH_BUCKET(BKT, PTR, STR);					       \
+    LFHT_LOCAL_BUCKET_BUFFER = (((void**)V04_UNTAG(PTR)) - 1);                \
+  }
+
+#else /* !LFHT_LOCAL_THREAD_BUFFER_FOR_BUCKET_ARRAYS */
+
+#define V04_ALLOC_BUCKETS(BUCKET_PTR, PREV_HASH, STR)	                             \
+  { void **alloc_bucket_ptr;						             \
+    V04_ALLOC_THB(alloc_bucket_ptr);					             \
+    V04_INIT_BUCKETS(alloc_bucket_ptr, PREV_HASH);                                   \
+    BUCKET_PTR = (STR **) alloc_bucket_ptr;                                          \
+  }
+
+#define V04_FREE_TRIE_HASH_BUCKETS(PTR, BKT, STR)				     \
+   V04_FREE_THB(((STR *) V04_UNTAG(PTR)) - 1) /* DOES NOT DO ANYTHING */
+  /*  FREE_BLOCK(((ans_node_ptr *) V04_UNTAG(STR)) - 1) */
+
+
+#endif /* LFHT_LOCAL_THREAD_BUFFER_FOR_BUCKET_ARRAYS */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #define V04_SHIFT_ENTRY(ENTRY, N_SHIFTS)               ((ENTRY) >> ((SHIFT_SIZE * (N_SHIFTS)) + LFHT_NrLowTagBits))
 #define V04_HASH_ENTRY(ENTRY, N_SHIFTS)                (V04_SHIFT_ENTRY(ENTRY, N_SHIFTS) & (BASE_HASH_BUCKETS - 1))
 #define V04_IS_EMPTY_BUCKET(BUCKET, BASE_BUCKET, STR)  (BUCKET == (STR *) BASE_BUCKET)
-#define V04_TAG(PTR)                                   ((long)(PTR) | (long)0x1)
-#define V04_UNTAG(PTR)                                 ((long)(PTR) & ~(long)(0x1))
 
 
 #define V04_GET_HASH_BUCKET(BUCKET, HASH, T, NS, STR)  (BUCKET = (STR **) V04_UNTAG(HASH) + V04_HASH_ENTRY((long)T, NS))
 #define V04_GET_PREV_HASH(PREV_HASH, CURR_HASH, STR)   (PREV_HASH = (STR **) *(((STR **) V04_UNTAG(CURR_HASH)) - 1))
 #define V04_SET_HASH_BUCKET(BUCKET, V, STR)            (*(BUCKET) = (STR *) V)
 #define LFHT_BOOL_CAS(PTR, OLD, NEW)                    __sync_bool_compare_and_swap((PTR), (OLD), (NEW))
+
+
+
+
+
+
+
+
+
+
 
 
 
--- OPTYap/lockFreeHash.tries.i
+++ OPTYap/lockFreeHash.tries.i
@@ -7,7 +7,6 @@
 #define LFHT_USES_ARGS1                   sg_node_ptr parent_node LFHT_USES_REGS
 #define LFHT_PASS_ARGS                    , parent_node LFHT_PASS_REGS
 #define LFHT_ROOT_ADDR                    (&(TrNode_child(parent_node)))
-#define LFHT_GET_FIRST_NODE(NODE)         (NODE = (LFHT_STR_PTR) TrNode_child(parent_node))
 #define LFHT_NEW_NODE(NODE, ENTRY, NEXT)  {NEW_SUBGOAL_TRIE_NODE(NODE, ENTRY, NULL, parent_node, NEXT);}
 #define LFHT_FREE_NODE(PTR)               FREE_SUBGOAL_TRIE_NODE(PTR);
 #undef LFHT_STR
@@ -17,7 +16,6 @@
 #undef LFHT_USES_ARGS1
 #undef LFHT_PASS_ARGS
 #undef LFHT_ROOT_ADDR
-#undef LFHT_GET_FIRST_NODE
 #endif /* INCLUDE_SUBGOAL_LOCK_FREE_HASH_TRIE */
 
 #ifdef INCLUDE_ANSWER_LOCK_FREE_HASH_TRIE
@@ -27,7 +25,6 @@
 #define LFHT_USES_ARGS1                   ans_node_ptr parent_node, int instr LFHT_USES_REGS
 #define LFHT_PASS_ARGS                    , parent_node, instr LFHT_PASS_REGS
 #define LFHT_ROOT_ADDR                    (&(TrNode_child(parent_node)))
-#define LFHT_GET_FIRST_NODE(NODE)         (NODE = (LFHT_STR_PTR) TrNode_child(parent_node))
 #define LFHT_NEW_NODE(NODE, ENTRY, NEXT)  {NEW_ANSWER_TRIE_NODE(NODE, instr, ENTRY, NULL, parent_node, NEXT);}
 #define LFHT_FREE_NODE(PTR)               FREE_ANSWER_TRIE_NODE(PTR)
 #undef LFHT_STR
@@ -37,7 +34,6 @@
 #undef LFHT_USES_ARGS1
 #undef LFHT_PASS_ARGS
 #undef LFHT_ROOT_ADDR
-#undef LFHT_GET_FIRST_NODE
 #endif /* INCLUDE_ANSWER_LOCK_FREE_HASH_TRIE */
 
 ####################################################################################
@@ -66,14 +62,14 @@ External dependencies:
 /* answer_trie_check_insert_entry */
 static inline LFHT_STR_PTR lfht_check_insert_entry(LFHT_NODE_ENTRY_STR entry LFHT_USES_ARGS) {
   LFHT_STR_PTR first_node;
-  LFHT_GET_FIRST_NODE(first_node);
+  LFHT_GetFirstNode(first_node);
   if (first_node == NULL) {
     LFHT_STR_PTR new_node;
     LFHT_NEW_NODE(new_node, entry, NULL);
     if (LFHT_BOOL_CAS(LFHT_ROOT_ADDR, NULL, new_node))
       return new_node;
     LFHT_FREE_NODE(new_node);
-    LFHT_GET_FIRST_NODE(first_node);
+    LFHT_GetFirstNode(first_node);
   }  
   if (LFHT_IsHashLevel(first_node))
     return lfht_check_insert_bucket_array((LFHT_STR_PTR *) first_node, entry, 0 LFHT_PASS_ARGS);
@@ -88,26 +84,26 @@ static inline LFHT_STR_PTR lfht_check_insert_first_chain(LFHT_STR_PTR chain_node
     return chain_node;  
   int cn = count_nodes + 1;
   LFHT_STR_PTR chain_next;
-  chain_next = LFHT_NodeNext(chain_node); // HERE
+  chain_next = LFHT_NodeNext(chain_node); 
 
-  if (chain_next && !V04_IS_HASH(chain_next))
-    return answer_trie_check_insert_first_chain(chain_next, parent_node, t, instr, cn PASS_REGS);  
+  if (chain_next && !LFHT_IsHashLevel(chain_next))
+    return lfht_check_insert_first_chain(chain_next, entry, cn LFHT_PASS_ARGS); 
   
   // chain_next is a hash pointer or the end of the chain
   if (chain_next == NULL) {
-    if (cn == MAX_NODES_PER_BUCKET) {
-      ans_node_ptr *new_hash;
-      ans_node_ptr *bucket;
-      V04_ALLOC_BUCKETS(new_hash, NULL, struct answer_trie_node);
+    if (cn == LFHT_MAX_NODES_PER_BUCKET) {  
+      LFHT_STR_PTR *new_hash;
+      LFHT_STR_PTR *bucket;                                                    // HERE
+      LFHT_AllocBuckets(new_hash, NULL, LFHT_STR);
       new_hash = (ans_node_ptr *) V04_TAG(new_hash);
-      V04_GET_HASH_BUCKET(bucket, new_hash, TrNode_entry(chain_node), 0, struct answer_trie_node);
-      V04_SET_HASH_BUCKET(bucket, chain_node, struct answer_trie_node);
+      V04_GET_HASH_BUCKET(bucket, new_hash, TrNode_entry(chain_node), 0, LFHT_STR);
+      V04_SET_HASH_BUCKET(bucket, chain_node, LFHT_STR);
       if (BOOL_CAS(&TrNode_next(chain_node), NULL, new_hash)) {
 	answer_trie_adjust_chain_nodes(new_hash, TrNode_child(parent_node), chain_node, (- 1) PASS_REGS);
 	TrNode_child(parent_node) = (ans_node_ptr) new_hash;
 	return answer_trie_check_insert_bucket_array(new_hash, parent_node, t, instr, 0 PASS_REGS);
       } else 
-	V04_FREE_TRIE_HASH_BUCKETS(new_hash, bucket, struct answer_trie_node);  
+	V04_FREE_TRIE_HASH_BUCKETS(new_hash, bucket, LFHT_STR);  
     } else {
       ans_node_ptr new_node; 
       NEW_ANSWER_TRIE_NODE(new_node, instr, t, NULL, parent_node, NULL);
@@ -122,10 +118,10 @@ static inline LFHT_STR_PTR lfht_check_insert_first_chain(LFHT_STR_PTR chain_node
   // chain_next is pointig to an hash which is newer than mine. I must jump to the correct hash
   ans_node_ptr *jump_hash, *prev_hash;
   jump_hash = (ans_node_ptr *) chain_next;
-  V04_GET_PREV_HASH(prev_hash, jump_hash, struct answer_trie_node);
+  V04_GET_PREV_HASH(prev_hash, jump_hash, LFHT_STR);
   while (prev_hash != NULL) {
     jump_hash = prev_hash;
-    V04_GET_PREV_HASH(prev_hash, jump_hash, struct answer_trie_node);
+    V04_GET_PREV_HASH(prev_hash, jump_hash, LFHT_STR);
   }
   return answer_trie_check_insert_bucket_array(jump_hash, parent_node, t, instr, 0 PASS_REGS);  
 } 
